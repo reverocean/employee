4075fa7aef82602b4b626b2bb39902e006b41434
reverocean <reverocean@gmail.com>
2014-11-26 23:15:38 +0800

    Update Te.java

--- src/main/java/employee/Te.java
+++ src/main/java/employee/Te.java
@@ -1,9 +1,7 @@
 package employee;
 
 public class Te {
-    private final int id;
-    private final String name;
-
+    
     public Te(int id, String name) {
         throw new UnsupportedOperationException();
     }
